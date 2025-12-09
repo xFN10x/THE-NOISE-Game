7a7775f805022e9134052a1c07fef7fd3a3a01e7
_FN10_ <mathdudex09@gmail.com>
2025-12-09 18:11:32 -0400

    add fonts, menu transition, and add intro logo skipping

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -19,17 +19,38 @@ public class MenuHandler : MonoBehaviour
 
     public List<Texture> Logos;
 
+    private GameObject CurrentMenu;
     private InputAction PointAction;
+    private InputActionAsset Actions;
 
     private void Start()
     {
+        Actions = GetComponent<PlayerInput>().actions;
+        CurrentMenu = MakeActiveAfterLogos.gameObject;
         MakeActiveAfterLogos.gameObject.SetActive(false);
         MakeActiveAfterLogos.interactable = false;
         BGCoveringUIElement.gameObject.SetActive(true);
-        PointAction = GetComponent<PlayerInput>().actions.FindAction("Point");
+        PointAction = Actions.FindAction("Point");
+
+        Actions.FindAction("SkipCutscene").performed += SkipIntro;
 
         MainSource.time = MusicStartMessure * 2;
         StartCoroutine(StartingLogos());
+    }
+
+    private void SkipIntro(InputAction.CallbackContext obj)
+    {
+
+        MainSource.time = 24;
+        if (!MainSource.isPlaying)
+            MainSource.Play();
+        StopAllCoroutines();
+        MakeActiveAfterLogos.alpha = 0;
+        MakeActiveAfterLogos.gameObject.SetActive(true);
+        MakeActiveAfterLogos.interactable = true;
+        BGCoveringUIElement.DOColor(new Color(0, 0, 0, 0), 1);
+        MakeActiveAfterLogos.DOFade(1, 2f);
+        Actions.FindAction("SkipCutscene").performed -= SkipIntro;
     }
 
     private void Update()
@@ -41,7 +62,18 @@ public class MenuHandler : MonoBehaviour
             0);
     }
 
-    IEnumerator StartCutscene()
+    private IEnumerator SwitchMenu(GameObject menu)
+    {
+        yield return CurrentMenu.GetComponent<RectTransform>().DORotate(new Vector3(0, 90, 0), 0.5f).SetEase(Ease.Linear).WaitForCompletion();
+        CurrentMenu.SetActive(false);
+        CurrentMenu = menu;
+        //now, current menu is the new one
+        CurrentMenu.SetActive(true);
+        CurrentMenu.GetComponent<RectTransform>().rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+        yield return CurrentMenu.GetComponent<RectTransform>().DORotate(new Vector3(0, 0, 0), 0.5f).SetEase(Ease.Linear).WaitForCompletion();
+    }
+
+    private IEnumerator StartCutscene()
     {
         MainSource.time = 200;
         MakeActiveAfterLogos.DOFade(0, 3f);
@@ -52,12 +84,13 @@ public class MenuHandler : MonoBehaviour
         SceneManager.LoadSceneAsync("Game", LoadSceneMode.Single);
     }
 
-    IEnumerator StartingLogos()
+    private IEnumerator StartingLogos()
     {
         yield return new WaitForSeconds(2);
         MainSource.Play();
         if (MusicStartMessure == 0)
             yield return new WaitForSeconds(4);
+
         foreach (var tex in Logos)
         {
             GameObject UIElement = new(Logos.IndexOf(tex).ToString());
@@ -65,8 +98,9 @@ public class MenuHandler : MonoBehaviour
             img.texture = tex;
             img.SetNativeSize();
             img.color = new Color(255, 255, 255);
+            img.raycastTarget = false;
 
-            UIElement.transform.parent = MainCanvas.gameObject.transform;
+            UIElement.transform.SetParent(MainCanvas.transform);
             UIElement.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.5f, 0.5f);
 
             img.DOColor(new Color(255, 255, 255, 0), 3);
@@ -93,5 +127,10 @@ public class MenuHandler : MonoBehaviour
     {
         MakeActiveAfterLogos.interactable = false;
         StartCoroutine(StartCutscene());
+    }
+
+    public void SwitchMenu(CanvasGroup nextMenu)
+    {
+        StartCoroutine(SwitchMenu(nextMenu.gameObject));
     }
 }
